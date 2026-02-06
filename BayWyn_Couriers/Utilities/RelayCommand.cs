d8051de584eda01700fab290347d8ef9e37e1bbe
Levi Varughese Mathew <levivarghesemathew@gmail.com>
2026-02-06 22:50:55 +0000

    Update database files; simplify RelayCommand
    Update MDF/LDf database binaries and refactor RelayCommand.cs. The command class now allows a nullable _canExecute delegate (Func<object?, bool>?), removes the ArgumentNullException/default-delegate in the constructor, and defers executability checks to CanExecute which treats a null _canExecute as always-true. Added explanatory comments and minor formatting cleanups. No intended behavioral changes beyond simplifying null handling and constructor semantics.
    
    This RelayCommand was created from scratch referring to a youtube video. Although it is similar to the previous (backup) relay command, starting scratch made the code a bit more clear. Additionally, some cleanup is done.

--- BayWyn_Couriers/Utilities/RelayCommand.cs
+++ BayWyn_Couriers/Utilities/RelayCommand.cs
@@ -5,46 +5,45 @@ namespace BayWyn_Couriers.Utilities
     // This is the class that will be used to bind commands to the UI elements in the MVVM pattern. It implements the ICommand interface, allowing it to be used as a command source in WPF applications.
     class RelayCommand : ICommand
     {
-        // The _execute field holds the action that will be executed when the command is invoked. The _canExecute field holds a function that determines whether the command can execute in its current state.
-        // Both fields are initialized in the constructor, with _canExecute defaulting to a function that always returns true if it is not provided.
+        // Method that has a single parameter without a return value, which will be executed when the command is invoked.
         private readonly Action<object?> _execute;
-        private readonly Func<object?, bool> _canExecute;
+        // Func is similar to Action, but it has a return value.
+        // In this case, it takes an object? as a parameter and returns a bool,
+        // which will be used to determine if the command can be executed (else the button will be disabled).
+        private readonly Func<object?, bool>? _canExecute;
 
-        /// <summary>
-        /// Occurs when changes affect whether the command can execute.
-        /// </summary>
-        /// <remarks>This event is typically raised by the <see cref="CommandManager"/> to indicate that
-        /// the  execution status of the command may have changed. Handlers added to this event should  check the
-        /// command's ability to execute by calling the <c>CanExecute</c> method.</remarks>
+        // Auto code which automatically checks if the button should be enabled or not by subscribing to the CommandManager.RequerySuggested event,
+        // which is raised whenever the command manager detects conditions that might change the ability of a command to execute.
         public event EventHandler? CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        /// <summary>
-        /// Represents a command that can be executed in response to user interaction, with optional logic to determine
-        /// if the command can execute.
-        /// </summary>
-        /// <param name="execute">The action to execute when the command is invoked. This parameter cannot be <see langword="null"/>.</param>
-        /// <param name="canExecute">An optional function that determines whether the command can execute. If <see langword="null"/>, the command
-        /// is always executable.</param>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="execute"/> is <see langword="null"/>.</exception>
+
+        //Constructor that takes an Action and an optional Func. The Action is required, while the Func is optional and defaults to a function that always returns true if not provided.
         public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
         {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            _canExecute = canExecute ?? (_ => true);
+            _execute = execute;
+            _canExecute = canExecute;
         }
 
-        /// <summary>
-        /// Determines whether the command can execute in its current state.
-        /// </summary>
-        /// <param name="parameter">An optional parameter used by the command to evaluate its execution state. This can be <see
-        /// langword="null"/> if the command does not require a parameter.</param>
-        /// <returns><see langword="true"/> if the command can execute; otherwise, <see langword="false"/>.</returns>
-        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
+        // Function that determines whether the command can execute in its current state.
+        // If no function is provided for _canExecute, it returns true, allowing the command to execute.
+        // Otherwise, it evaluates the provided function with the given parameter to determine if the command can execute.
+        public bool CanExecute(object? parameter)
+        {
+            // If no function is given for _canExecute, the command returns true (_canExecute == null). Button will not be disabled.
+            // Otherwise, it evaluates the function with the given parameter to determine if the command can execute.
+            return _canExecute == null || _canExecute(parameter);
+        }
 
-        
-        public void Execute(object? parameter) => _execute(parameter);
+        // Method that executes the command by invoking the _execute action with the provided parameter.
+        // The parameter can be null and it is mostly used to pass data from the UI to the command when it is executed.
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
     }
+
 }
